80bbc671fd854a400b5c042e704dca4ee2141a33
k-wakisaka <k.wakisaka@vaikobo.com>
2023-07-05 13:53:54 +0900

    first commit

--- inference.py
+++ inference.py
@@ -42,7 +42,7 @@ def main(input_saved_model_dir_path, input_classes_path, input_video_dir_path, o
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='inference')
-    parser.add_argument('--input_saved_model_dir_path', type=str, default='~/.video-classification-pb-trainer/output_model/2023-07-05-12-06-57/step-1000_batch-8_epoch-5_loss_0.1981_sparse_categorical_accuracy_0.9427_val_loss_1.1117_val_sparse_categorical_accuracy_0.7121')
+    parser.add_argument('--input_saved_model_dir_path', type=str, default='~/.vaik-video-classification-pb-trainer/output_model/2023-07-05-13-32-38/step-1000_batch-8_epoch-2_loss_0.1856_sparse_categorical_accuracy_0.9485_val_loss_1.4722_val_sparse_categorical_accuracy_0.6000')
     parser.add_argument('--input_classes_path', type=str, default='~/.vaik-utc101-video-classification-dataset/ucf101_labels.txt')
     parser.add_argument('--input_video_dir_path', type=str, default='~/.vaik-utc101-video-classification-dataset/test')
     parser.add_argument('--output_json_dir_path', type=str, default='~/.vaik-video-classification-pb-experiment/test_inf')
