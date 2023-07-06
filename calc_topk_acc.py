4c2da41f9ed7fc08d75a509c298e5f4f59ac1506
k-wakisaka <k.wakisaka@vaikobo.com>
2023-07-06 09:31:56 +0900

    first commit

--- calc_topk_acc.py
+++ calc_topk_acc.py
@@ -12,7 +12,9 @@ def calc_topk_acc(top_k, json_dict_list, classes):
         classes_score = {}
         for inf_json_dict in json_dict['inf']:
             for label, score in zip(inf_json_dict['label'], inf_json_dict['score']):
-                classes_score[label] = score
+                if label not in classes_score.keys():
+                    classes_score[label] = 0
+                classes_score[label] += score
         inf_top3_list = sorted(classes_score.items(), key=lambda x: x[1], reverse=True)[:top_k]
         if json_dict['answer'] in [k for k, v in inf_top3_list]:
             inf_class_label = json_dict['answer']
@@ -40,8 +42,8 @@ def main(top_k, input_json_dir_path, input_classes_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='inference')
-    parser.add_argument('--top_k', type=int, default=3)
-    parser.add_argument('--input_json_dir_path', type=str, default='~/.vaik-video-classification-pb-experiment/test_inf')
+    parser.add_argument('--top_k', type=int, default=1)
+    parser.add_argument('--input_json_dir_path', type=str, default='~/.vaik-video-classification-pb-experiment/train_inf')
     parser.add_argument('--input_classes_path', type=str, default='~/.vaik-utc101-video-classification-dataset/sub_ucf101_labels.txt')
     args = parser.parse_args()
 
